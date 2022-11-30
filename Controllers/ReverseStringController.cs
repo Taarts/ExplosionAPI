02ae075a9a3c57659b24282433a1d54c62bc2408
Tricia Amheiser <tricia@taarts.net>
2022-11-29 20:22:35 -0500

    Well, it works

--- Controllers/ReverseStringController.cs
+++ Controllers/ReverseStringController.cs
@@ -11,7 +11,7 @@ namespace ExplosionAPI.Controllers
     [ApiController]
     public class ReverseStringController : ControllerBase
     {
-        [HttpGet]
+        [HttpGet("{str}")]
         public string RevString(string str)
         {
             char[] chars = str.ToCharArray();
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ExplosionAPI
+
+first trip into API
+Working with Code Wars that were solved in a previous session and creating controllers, using C# that solve the 3 katas.
